4f4e30da56818bd125c6dac5e14c85154130081c
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-12-07 13:53:29 -0700

    feat: add skeleton WC class (#2)

---
+++ src/wc.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+
+
+export class WC {
+  constructor() {}
+
+  logger(...args) {
+  }
+
+  fileReader(file) {
+  }
+
+  countBytes(file) {
+
+  }
+
+  countLines(file) {
+
+  }
+
+  countWords(file) {
+
+  }
+
+  countCharacters(file) {
+
+  }
+}
