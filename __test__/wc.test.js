e09408b7dacad8a36035ca0bb6902d1cf5f05ce7
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-12-11 14:10:34 -0700

    add wc testing (#4)
    * feat: add testing for wc class and mockedContents
    
    * feat: add logger, fileReader, countBytes, countLines, countWords, countChars functions

--- __test__/wc.test.js
+++ __test__/wc.test.js
@@ -1,20 +1,72 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { WC } from "../src/wc.js";
+import { mockedContents } from "./example.js";
+import fs from "fs";
 
-describe("checking custom wc tool", () => {
-	const wc = new WC();
+describe("WC class", () => {
+	let wc;
 
-	it("should return logged value", () => {
-		const log = console.log;
-		let loggedOutput = "";
-		console.log = (...args) => {
-			loggedOutput += args.join(" ");
-		};
+	beforeEach(() => {
+		wc = new WC();
+	});
 
-		wc.logger("ccwc -c test.txt");
+	describe("logger method", () => {
+		it("should log the passed message", () => {
+			const logSpy = vi.spyOn(console, "log");
 
-		console.log = log;
+			wc.logger("ccwc -l example.txt");
 
-		expect(loggedOutput).toContain("ccwc -l test.txt");
+			expect(logSpy).toHaveBeenCalledWith("ccwc -l example.txt");
+		});
+	});
+
+	describe("fileReader method", () => {
+		it("should return the file contents", () => {
+			vi.spyOn(fs, "readFileSync").mockReturnValue(mockedContents);
+
+			const result = wc.fileReader("file.txt");
+
+			expect(result).toBe(mockedContents);
+		});
+	});
+
+	describe("countBytes method", () => {
+		it("should return the file size in bytes", () => {
+			vi.spyOn(fs, "statSync").mockReturnValue({ size: 100 });
+
+			const result = wc.countBytes("file.txt");
+
+			expect(result).toBe(100);
+		});
+	});
+
+	describe("countLines method", () => {
+		it("returns correct line count for a string", () => {
+			vi.spyOn(wc, "fileReader").mockReturnValue(mockedContents);
+
+			const result = wc.countLines("");
+
+			expect(result).toBe(6);
+		});
+	});
+
+	describe("countWords method", () => {
+		it("returns correct word count for a string", () => {
+			vi.spyOn(wc, "fileReader").mockReturnValue(mockedContents);
+
+			const result = wc.countWords("");
+
+			expect(result).toBe(53);
+		});
+	});
+
+	describe("countCharacters method", () => {
+		it("returns correct character count for a string", () => {
+			vi.spyOn(wc, "fileReader").mockReturnValue(mockedContents);
+
+			const result = wc.countCharacters("");
+
+			expect(result).toBe(346);
+		});
 	});
 });
--- src/wc.js
+++ src/wc.js
@@ -3,15 +3,38 @@ import fs from "fs";
 export class WC {
 	constructor() {}
 
-	logger(...args) {}
+	logger(...args) {
+		console.log(...args);
+	}
 
-	fileReader(file) {}
+	fileReader(file) {
+		return fs.readFileSync(file, "utf8");
+	}
 
-	countBytes(file) {}
+	countBytes(file) {
+		const stats = fs.statSync(file);
+		return stats.size;
+	}
 
-	countLines(file) {}
+	countLines(file) {
+		const contents = this.fileReader(file);
+		const lines = contents.split("\n");
+		let lineCounts = lines.length;
+		if (lines[lines.length - 1] === "") {
+			lineCounts -= 1;
+		}
 
-	countWords(file) {}
+		return lineCounts;
+	}
 
-	countCharacters(file) {}
+	countWords(file) {
+		const contents = this.fileReader(file);
+		const words = contents.split(/\s+/).filter((word) => word !== "");
+		return words.length;
+	}
+
+	countCharacters(file) {
+		const contents = this.fileReader(file);
+		return contents.length;
+	}
 }
