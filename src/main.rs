bd3d8d6e0cd244b27ff06b47976c1071c18d6394
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-02-09 08:57:09 -0700

    add tests (#2)
    * chore: add 2 test files for quotes
    
    * chore: add env and print out args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() < 1 {
+        println!("Usage: cargo run -- <file>");
+        std::process::exit(1)
+    }
+
+    println!("{:?}", args)
 }
