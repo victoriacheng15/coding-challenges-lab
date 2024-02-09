533b523b81dd383f9d40187a2b663d37a721490e
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-02-09 10:05:02 -0700

    chore: complete step 4 - add display line number with -n (#6)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use std::{env, fs, io};
 
-fn process_input(contents: String) {
+fn process_input(contents: String, display_line_number: bool) {
+    let mut line_number = 1;
+
     for line in contents.lines() {
         let trimmed_line = line.trim();
-        println!("{trimmed_line}")
+
+        if display_line_number {
+            println!("{: >6} {}", line_number, trimmed_line);
+            line_number += 1
+        } else {
+            println!("{trimmed_line}")
+        }
     }
 }
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().skip(1).collect();
+    let display_line_number = args.iter().any(|arg| arg == "-n");
 
     if args.len() < 1 {
         println!("Usage: cargo run -- <file>");
@@ -18,16 +27,17 @@ fn main() -> Result<(), io::Error> {
     // println!("{:?}", args);
 
     for filename in args {
-        if filename == "-" {
+        if filename == "-" || filename == "-n" {
             let mut contents = String::new();
             for line in io::stdin().lines() {
                 contents.push_str(&line?);
                 contents.push_str("\n");
             }
-            println!("{contents}")
+            // println!("{filename}");
+            process_input(contents, display_line_number)
         } else {
             let contents = fs::read_to_string(filename)?;
-            process_input(contents)
+            process_input(contents, display_line_number)
         }
     }
 
