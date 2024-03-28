95ce0e07ef215b4c03b9f0a2d905016dc76d7ce6
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-03-28 10:49:04 -0600

    feat: add ability to check field and delimter options, if exists, extract the filed numbers and delimiter (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, io, process};
+use std::{env, fs, process};
 
 #[allow(unused_imports)]
 
@@ -25,21 +25,58 @@ fn parse_config(args: &[String]) -> Config {
         process::exit(1);
     }
 
-    println!("{:?}", args);
+    // println!("{:?}", args);
     Config { options, filename }
 }
 
-fn main() -> Result<(), io::Error> {
+fn get_field_numbers(options: &[String]) -> Vec<u32> {
+    let has_field = options.iter().any(|opt| opt.starts_with("-f"));
+    let mut field_number = Vec::new();
+    if has_field {
+        let field = options
+            .iter()
+            .find(|opt| opt.starts_with("-f"))
+            .unwrap()
+            .split("f")
+            .nth(1)
+            .unwrap();
+
+        for num in field.chars() {
+            if num.is_digit(10) {
+                field_number.push(num.to_digit(10).unwrap());
+            }
+        }
+    }
+
+    field_number
+}
+
+fn get_delimiter(options: &[String]) -> String {
+    let _has_delimiter = options.iter().any(|opt| opt.starts_with("-d"));
+
+    //  wait to extract the delimiter for later
+
+    return ",".to_string();
+}
+
+fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let config = parse_config(&args);
 
+    let field_numbers = get_field_numbers(&config.options);
+    let delimiter = get_delimiter(&config.options);
+
+    println!(
+        "field_number: {:?}, delimiter: {}",
+        field_numbers, delimiter
+    );
+
     let mut all_contents = String::new();
-
-    let contents = fs::read_to_string(&config.filename)?;
+    let contents = fs::read_to_string(&config.filename).expect("Failed to read the file");
     all_contents.push_str(&contents);
-
-    println!("{}", contents);
-    println!("{:?}", config.options);
-
-    Ok(())
+    for line in contents.lines() {
+        if let Some(column) = line.split_whitespace().nth(1 - 1) {
+            println!("{}", column);
+        }
+    }
 }
