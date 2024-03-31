a91189fc118b4084570f9163f0d0aad7bd676bf9
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-03-31 15:28:05 -0600

    feat: get_delimiter now return the delimiter or an empty one (#5)

--- src/libs/delimiter.rs
+++ src/libs/delimiter.rs
@@ -1,7 +1,9 @@
 pub fn get_delimiter(options: &[String]) -> String {
-    let _has_delimiter = options.iter().any(|opt| opt.starts_with("-d"));
+    if let Some(delimiter_option) = options.iter().find(|&opt| opt.starts_with("-d")) {
+        if let Some(delimiter) = delimiter_option.strip_prefix("-d") {
+            return delimiter.to_string();
+        }
+    }
 
-    //  wait to extract the delimiter for later
-
-    return ",".to_string();
+    "".to_string()
 }
