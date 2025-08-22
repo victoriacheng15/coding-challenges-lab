92b07af6905ed4350c2508cb11e3b7651ecffda9
Victoria Cheng <victoria.cheng15@gmail.com>
2025-08-22 10:47:58 -0400

    add step 1 - read content from files

--- go/cat-go/cmd/root.go
+++ go/cat-go/cmd/root.go
@@ -1,34 +1,54 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "cat-go",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+type Flags struct {
+	numberNonBlank bool
+	numberLines    bool
+	showEnds       bool
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+var catFlags Flags
+
+var rootCmd = &cobra.Command{
+	Use:   "cat-go [flag] [file...]",
+	Short: "Concatenate files and print to standard output",
+	Long: `Concatenate and print files to standard output. It can be used to display the content of one or more files. If no file is specified, it reads from standard input.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("No files specified, reading from stdin")
+			return
+		}
+
+		for _, filename := range args {
+			file, err := os.Open(filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error opening file %s: %v\n", filename, err)
+				continue
+			}
+			defer file.Close()
+
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				line := scanner.Text()
+				// TODO: Add flag logic here later
+				// For now, just print the line
+				fmt.Println(line)
+			}
+
+			if err := scanner.Err(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error reading file %s: %v\n", filename, err)
+			}
+		}
+	},
+}
+
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -37,15 +57,9 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cat-go.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&catFlags.numberNonBlank, "number-nonblank", "b", false, "Number non-empty output lines")
+	rootCmd.Flags().BoolVarP(&catFlags.numberLines, "number", "n", false, "Number all output lines")
+	rootCmd.Flags().BoolVarP(&catFlags.showEnds, "show-ends", "E", false, "Display $ at end of each line")
 }
 
 
