544701c58fd46127f3c6147d795abad2ecedecda
Victoria Cheng <victoria.cheng15@gmail.com>
2025-08-23 10:37:21 -0400

    add 3 flags, remove dup, sort method and randomsort

--- go/sort-go/cmd/root.go
+++ go/sort-go/cmd/root.go
@@ -13,7 +13,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type Flags struct {
+	RemoveDuplicates bool
+	SortMethod       string
+	RandomSort       bool
+}
 
+var sortFlags Flags
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -56,8 +62,6 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -66,15 +70,9 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sort-go.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&sortFlags.RemoveDuplicates, "remove-duplicates", "u", false, "Remove duplicate lines")
+	rootCmd.Flags().StringVarP(&sortFlags.SortMethod, "sort-method", "s", "merge", "Sorting method (merge, quick, binarysearch)")
+	rootCmd.Flags().BoolVarP(&sortFlags.RandomSort, "random-sort", "R", false, "Randomize output order")
 }
 
 
